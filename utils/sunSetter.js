ae4ccf90898258a7e8238815013f95350c3d96f4
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-01-29 17:14:08 -0600

    Added sun-position-setter utility

---
+++ utils/sunSetter.js
@@ -0,0 +1,28 @@
+AFRAME.registerComponent('sun-position-setter', {
+        init: function () {
+          var skyEl = this.el;
+          var orbitEl = this.el.sceneEl.querySelector('#orbit');
+
+          orbitEl.addEventListener('componentchanged', function changeSun (evt) {
+            var sunPosition;
+            var phi;
+            var theta;
+
+            if (evt.detail.name !== 'rotation') { return; }
+
+            sunPosition = orbitEl.getAttribute('rotation');
+
+            if(sunPosition === null) { return; }
+
+            theta = Math.PI * (- 0.5);
+            phi = 2 * Math.PI * (sunPosition.y / 360 - 0.5);
+			//Debugging position
+			console.log(Math.cos(phi), Math.sin(phi) * Math.sin(theta))
+            skyEl.setAttribute('material', 'sunPosition', {
+              x: Math.cos(phi),
+              y: Math.sin(phi) * Math.sin(theta),
+              z: -1
+            });
+          });
+        }
+      });
